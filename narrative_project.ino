d07e1be113e38cd942821a0eccbb264a18e5c045
natalie sottek <nataliesottek@utexas.edu>
2025-11-12 15:32:56 -0600

    Update narrative_project.ino

--- narrative_project.ino
+++ narrative_project.ino
@@ -27,3 +27,4 @@ void loop() {
 //inset code for scene 3: person put a mini chef hat on remy, triggers remy's arms to pull linguini's hair
 //insert code for scene 4: person moves small chef guy to a lightswitch on the wall, triggers the LED to turn on behind linguini's hat, shows Remy shadow. 
 //insert code for scene 5: option 1: person takes Linguini's hat off and the little chef turns around and sees Remy, option 2: person moves likttle chef to door, door "opens" and reveals pantry with rats, option 3: person moves littel chef to pantry door, physical door opens and reveals 3D pantry. 
+.
