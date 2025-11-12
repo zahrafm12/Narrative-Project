85cd9907e24e7364832c5e842a1f6d7f52158c9b
natalie sottek <nataliesottek@utexas.edu>
2025-11-12 15:31:55 -0600

    Update narrative_project.ino

--- narrative_project.ino
+++ narrative_project.ino
@@ -10,3 +10,20 @@ void loop() {
 
 //insert code or smthn
 //insert code to bring remy across water :D
+
+
+
+
+
+
+
+
+
+
+
+
+//insert code for scene 1: person pulls boat across water, interaction turns LED in the next scene ON
+//insert code for scene 2: person pulls the jar up (with remy in it) to trigger servo moves to show linguini holding remy outside the jar.
+//inset code for scene 3: person put a mini chef hat on remy, triggers remy's arms to pull linguini's hair
+//insert code for scene 4: person moves small chef guy to a lightswitch on the wall, triggers the LED to turn on behind linguini's hat, shows Remy shadow. 
+//insert code for scene 5: option 1: person takes Linguini's hat off and the little chef turns around and sees Remy, option 2: person moves likttle chef to door, door "opens" and reveals pantry with rats, option 3: person moves littel chef to pantry door, physical door opens and reveals 3D pantry. 
